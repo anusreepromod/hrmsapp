f48065a22bb162afed3c584c56bfb8e6df29b5e7
anusree promod <anusreepi@gmail.com>
2021-07-20 16:08:20 +0530

    version1

---
+++ userapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls.conf import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.login),
+    path('dashboard/', views.dashboard),
+    path('editprofile/', views.editprofile),
+    path('leave/', views.leave),
+    path('applyleave/', views.applyleave),
+    path('notification/', views.notification),
+    path('leavedetail/', views.leavedetail),
+    path('userprofile/', views.userprofile),
+    path('settings/', views.settings),
+
+]
---
+++ userapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def editprofile(request):
+    return render(request, 'editprofile.html')
+
+
+def leave(request):
+    return render(request, 'leaveapplication.html')
+
+
+def applyleave(request):
+    return render(request, 'applyleave.html')
+
+
+def notification(request):
+    return render(request, 'notification.html')
+
+
+def leavedetail(request):
+    return render(request, 'leavedetail.html')
+
+
+def userprofile(request):
+    return render(request, 'userprofile.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
