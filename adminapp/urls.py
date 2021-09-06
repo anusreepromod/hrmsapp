3a5c9297809881c628de1cb93225e19e3f078de6
anusreepromod <anusreepi@gmail.com>
2021-09-06 15:35:20 +0530

    version11

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,6 +4,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
+    path('logins/', views.logins),
     path('employee/', views.employee),
     path('addstaff/', views.addstaff),
     path('editemployee/', views.editemployee),
@@ -13,6 +14,16 @@ urlpatterns = [
     path('resignation/', views.resignation),
     path('meetings/', views.meetings),
     path('notifications/', views.notifications),
+    path('promotion/', views.promotion),
+    path('complaints/', views.complaints),
+    path('holidays/', views.holidays),
+    path('dashboards/', views.dashboards),
+    path('addcomplaints/', views.addcomplaints),
+    path('createresignation/', views.createresignation),
+    path('createpromotion/', views.createpromotion),
+    path('addmeetings/', views.addmeetings),
+    path('createleave/', views.createleave),
+
 
 
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def logins(request):
+    return render(request, 'logins.html')
+
+
 def employee(request):
     return render(request, 'employee.html')
 
@@ -37,3 +41,39 @@ def meetings(request):
 
 def notifications(request):
     return render(request, 'notifications.html')
+
+
+def promotion(request):
+    return render(request, 'promotion.html')
+
+
+def complaints(request):
+    return render(request, 'complaints.html')
+
+
+def holidays(request):
+    return render(request, 'holidays.html')
+
+
+def dashboards(request):
+    return render(request, 'dashboards.html')
+
+
+def addcomplaints(request):
+    return render(request, 'addcomplaints.html')
+
+
+def createresignation(request):
+    return render(request, 'createresignation.html')
+
+
+def createpromotion(request):
+    return render(request, 'createpromotion.html')
+
+
+def addmeetings(request):
+    return render(request, 'addmeetings.html')
+
+
+def createleave(request):
+    return render(request, 'createleave.html')
